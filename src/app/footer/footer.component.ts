3666541a0ee92d5c85d437c47d843445c8d401c8
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-06-11 22:51:42 +0530

    day 2

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,8 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+// ng g c footer -it -is
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html', // html
+  template: `
+    <div class="text-center">
+      <hr>
+      <app-menu>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Back to top</a>
+        </li>
+      </app-menu>
+      <p>Copyright &copy; 2020 | Hexaware </p>
+    </div>
+  `,
   styles: [
   ]
 })
