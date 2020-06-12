ca7dac22d3df79a560f53a4e8d567407536c118c
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-06-13 02:55:57 +0530

    day 3

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
 })
 export class ContactService {
 
-  REST_API_URL = 'https://reqres.in/api/users';
+  private REST_API_URL = 'http://jsonplaceholder.typicode.com/users';
 
   constructor( private http: HttpClient ) { }
 
@@ -28,6 +28,16 @@ export class ContactService {
           return res;
         }));
 
+  }
+
+  getContacts(){ // 1. get the req from comp
+
+    // 2. send the req to the REST api -- with get method using HttpClient
+    return this.http.get(this.REST_API_URL)
+      .pipe( map ((res: any[]) => { // 3. get the resp from the REST API 
+        console.log(res);
+        return res; // 4. send it back to the comp
+      }));
   }
 
 }
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'app-contacts',
@@ -8,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
-  constructor() { }
+  contactList: any[];
+
+  constructor( private contactService: ContactService) {
+    console.log('Inside constructor');
+  }
 
   ngOnInit(): void {
+    console.log('Inside ngOnInit');
+    // ideal place for making ajax calls
+    // 1. connect to the service -- ref constructor
+    // 2. send a call to the service method
+    this.contactService.getContacts()
+      .subscribe( (res: any[]) => { // 3. get the resp from the service
+        console.log(res);
+        this.contactList = res;
+      })
+    
   }
 
 }
