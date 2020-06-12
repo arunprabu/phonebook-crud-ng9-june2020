154192d54f6af04130d963fc70eda45335bde534
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-06-13 01:12:23 +0530

    day 3

--- src/app/contacts/add-contact/add-contact.component.ts
+++ src/app/contacts/add-contact/add-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
@@ -8,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddContactComponent implements OnInit {
 
+  // Step 1: Create Form tag equivalent in TS file
+  contactForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    // Step 1.. continues...
+    this.contactForm = new FormGroup({
+      // Step 2: Create form elements equivalent in TS file
+      name: new FormControl('', Validators.required),  // Step 5: Work with validators 
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+    // Step 3: ref html
+  }
+
+  addContactHandler(){
+    console.log(this.contactForm);
   }
 
 }
